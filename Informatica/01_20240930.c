91d32654966c68826b0fe9708ca91106ec3d2750
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2024-09-30 11:11:50 +0000

    QUADRATO

---
+++ Informatica/01_20240930.c
@@ -0,0 +1,23 @@
+/*DATI DUE NUMERI A e B VERIFICARE
+     SE A è IL QUADRATO DI B */
+
+#include <stdio.h>
+int main(){
+    float A=0;
+    float B=0;
+    float quadrato=0;
+
+    printf("Inserisci il primo valore: ");
+    scanf("%f , &A");
+    primo("Inserisci il secondo valore: ");
+    scanf("%f , &B");
+    quadrato = B*B;
+    printf("Il doppio è : %f", quadrato);
+    if(A == quadrato){
+
+     printf("Il primo valore %f corrisponde al quadrato del seocndo %f", A,B);
+    }
+    else{
+        printf("Il primo valore %f non corrisponde al quadrato del secondo %f" , A,B);
+    }
+    
