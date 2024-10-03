fd5b51a2e0036751e24f50d6b55fbd6e6f85f9c8
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2024-10-03 06:55:05 +0000

    01-20241003

---
+++ 01-20241003.c
@@ -0,0 +1,83 @@
+/* Chiedi all'utente 5 numeri e mostrali in oridine descrescente*/
+
+#include <stdio.h>
+int main(){
+
+    int n1, n2, n3, n4, n5, t;
+    printf("Inserici il primo valore");
+    scanf("%d", &n1);
+    printf("Inserisci il secondo valore");
+    scanf("%d", &n2);
+    printf("Inserisci il terzo valore");
+    scanf("%d, &n3");
+    printf("Inserici il quarto valore");
+    scanf("%d", &n4);
+    printf("Inserici il quinto valore");
+    scanf("%d", &n4);
+
+    if(n2>n1){
+        t=n1;
+        n1=n2;
+        n2=t;
+    }
+
+    if(n3>n1){
+        t=n1;
+        n3=n1;
+        n3=t;
+    }
+
+    if(n4>n1){
+        t=n1;
+        n4=n1;
+        t=n4;
+    }
+
+    if(n5=n1){
+        t=n1;
+        n5=n1;
+        t=n5;
+    }
+
+    if(n3>n2){
+        t=n2;
+        n3=n2;
+        t=n3
+    }
+     if(n4>n2){
+        t=n2;
+        n2=n4;
+        n4=t;
+    }
+
+    if(n5>n2){
+        t=n2;
+        n2=n5;
+        t=n5;
+    }
+
+    if(n4=n3){
+        t=n3;
+        n3=n4;
+        t=n4;
+    }
+
+    if(n5>n3){
+        t=n3;
+        n3=n5;
+        t=n5;
+    }
+
+    if(
+
+    ){
+
+
+    }
+
+
+
+
+    
+
+}
