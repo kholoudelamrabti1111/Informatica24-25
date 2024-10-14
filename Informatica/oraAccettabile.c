0e4904e2fcd9114825735f6bd4cc107e9b72589c
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2024-10-14 11:38:11 +0000

    01_20241014

--- Informatica/oraAccettabile.c
+++ Informatica/oraAccettabile.c
@@ -2,9 +2,33 @@
 #include <stdio.h>
 
 int main(){
-    int orario=0;
+    int ora=0;
+    int minuti=0;
+    int secondi=0;
 
-    printf("Inserisci l'orario");
-    scanf("%d", &orario);
+    printf("Inserisci l'ora");
+    scanf("%d", &ora);
+    printf("Inserisci i minuti");
+    scanf("%d", &minuti);ù
+    printf("Inserisci i secondi");
+    scanf("%d", &secondi);
+
+    if(ora>=0 && ora<24){
+        if(minuti>=0 && minuti<60)
+            if(secondi>=0 && secondi<60)
+                printf("L'orario è accettabile");
+
+        else{
+            printf("L'orario non è accettabile");
+        } 
+        else{
+            printf("L'orario non è accettabile");
+        }   
+        else{
+            printf("L'orario non è accettabile");
+        }    
+
+    }
+
 
 }
