f14068b397456dc4567d56ad79ab388baa6a4359
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2024-12-05 14:48:56 +0000

    ese7

---
+++ Informatica/Iterazioni/ese7_20241205asterischi.c
@@ -0,0 +1,13 @@
+/*Scrivi un programma che disegna sullo schermo 3 cornici quadrate concentriche 
+costituite dal carattere “*” rispettivamente di lato 12, 8, e 4.*/
+
+#include <stdio.h>
+int main(){
+    for (int i=0; i<8; i++){
+        for(int =0; j<8; j++ ){
+            if(j==0 || i==7 || i==j){
+                printf("")
+            }
+        }
+    }
+}
