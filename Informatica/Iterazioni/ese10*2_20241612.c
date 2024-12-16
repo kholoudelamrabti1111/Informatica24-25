c8a43444378a6fcf34220735c381d7d29d2f943b
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2024-12-16 10:54:05 +0000

    ese10

---
+++ Informatica/Iterazioni/ese10*2_20241612.c
@@ -0,0 +1,22 @@
+/*Stampa i primi N numeri dispari successivi al numero A*/
+
+#include <stdio.h>
+int main(){
+    int n=0, a=0;
+
+    do{
+        printf("Inserisci un numero");
+        scanf("%d", &n);
+        printf("Inserisci un numero");
+        scanf("%d", &a);
+    }while(n<0);
+
+    if(a%2==0){
+        if(a%2==1)
+    }
+
+    for(int i=0; i<n; i++){
+
+    }
+    
+}
