beb73dff9f06129bc0276d2fd76f5d92beb83e05
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2025-02-06 09:34:19 +0000

    esercizio 3

--- Informatica/funzioni.c/esercizio3DecBin.c
+++ Informatica/funzioni.c/esercizio3DecBin.c
@@ -1,17 +1,17 @@
 /*Dato un valore eseguire la conversione da decimale a binario*/
 
 #include <stdio.h>
-#include <mat.h>
+#include <math.h>
 long converti(int _n);
 int main(){
     int n=0;
     long conv=0;
     do{
         printf("Inserisci un valore: ");
-        scanf("%d", %n);
+        scanf("%d", &n);
 
     }while(n<=0);
-    conv=converti(int n);
+    conv=converti(n);
     printf("%ld\n", conv);
 }
 
@@ -19,7 +19,7 @@ long converti(int _n){
     int quoz=_n;
     int resto=0;
     int cont=0;
-    int _conv=0;//copia del conv che c'è nel main
+    long _conv=0;//copia del conv che c'è nel main
     while(quoz!=0){
         resto=quoz%2;//18|2=9, con resto 0
         quoz=quoz/2; //18|2=9 poi il 9|2 quoz cambia numero
