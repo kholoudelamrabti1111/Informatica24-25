da3e6ab7b5715dd9f99c77fc7e74f7fe21d5d750
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2026-03-17 08:52:49 +0000

    ordinedecrescente

---
+++ Info25_26/file.c/ordinedecrescente.c
@@ -0,0 +1,37 @@
+/*Scrivere una funzione che preso in ingresso
+il nome del file contenente interi, 
+lo modifichi in modo tale da contenere gli stessi
+numeri ma ordinati in senso decrescente
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+//LEGGERE, ORDINARE E SCRIVERE
+void OrdinaFile(char nomeFile[]){
+    FILE *Fp;
+    int vettore[100], i=0;
+
+    Fp = fopen(nomeFile, "r");
+    if(Fp== NULL){
+        printf("Errore apertura file");
+        return 1; 
+    }
+    //lettura dei numeri dal file
+    while(fscanf(Fp, "%d", &vettore[i]) == 1){//1 SE LEGGE UN NUMERO, 0 SE NON LEGGE 
+        printf("Elemento %d: %d\n", i+1, vettore[i]);
+        i++;
+    }
+    fclose(Fp);
+}
+
+
+int main(){
+    char nome[20];
+
+    printf("Inserisci il nome del file: ");//chiedo il nome del file all'utente
+    scanf("%s", nome);
+
+    OrdinaFile(nome);
+
+
+}
